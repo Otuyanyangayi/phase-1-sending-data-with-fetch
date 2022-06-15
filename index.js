63c25cb6b2d97beb850c367a7051403d2e85e5fe
Grace <gracenyangayi@gmail.com>
2022-06-15 08:18:13 +0300

    commit tree 3ce9fae9d26a8c15f2b510e27950d32ec377366c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData = () => {
+
+return fetch('http://localhost:3000/users', {
+    method : "POST",
+    headers: {
+        "Content-Type" : "application/json",
+        Accept : "application/json"
+    },
+    body:JSON.stringify({
+        name : "Steve",
+        email: "steve@steve.com"
+    }),
+})
+.then(res => res.json())
+.then( function ( object ) {
+    document.body.innerHTML = object[ "id" ]
+  } )
+.catch(function (error) {
+    document.body.innerHTML = error.message
+  });
+
+}
